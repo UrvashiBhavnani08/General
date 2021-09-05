bcc3512150e63f908658f2a038af4ef622107954
UrvashiBhavnani08 <90092579+UrvashiBhavnani08@users.noreply.github.com>
2021-09-05 10:11:24 +0530

    Add files via upload

---
+++ CreatingArrays.java
@@ -0,0 +1,20 @@
+class CreatingArrays
+{
+   public static void main(String args[])
+   {
+	int[] intArray;
+	intArray = new int[10];
+	// int sum;
+	int sum(int[] intArray);
+	{
+	int sum = 0;
+	for (int index = 0; index < intArray.length; index++) {
+		sum += intArray[index];
+	}
+	return sum;
+	}
+	
+	}
+	// System.out.println(sum);
+   }
+}
